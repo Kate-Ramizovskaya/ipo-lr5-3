8bd6b76e11aecd8b7f75e49fb58393c608a7f6a8
Kate Ramizovskaya <kr978407@gmail.com>
2024-10-18 15:05:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-with open('text.txt', 'r', encoding='utf-8') as f:
-    str=f.read()
-words=str.split()
-lenght=len(words)
-print("Кол-во слов в файле: ", lenght)
+with open('text.txt', 'r', encoding='utf-8') as f: #открытие файла для дальнейшего чтения
+    str=f.read() #присваивание переменной str открытому файлу
+words=str.split() #присваивание переменной words через str
+lenght=len(words) #перебор и подсчет переменной words
+print("Кол-во слов в файле: ", lenght) #вывод кол-ва
